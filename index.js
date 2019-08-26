4eb6270db2c38e719e5bcc79b7483a8080b6becc
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-08-26 18:31:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function prependDriver(name) {
 }
 
 function removeLastDriver() {
-  return [].concat(drivers.slice(0,drivers.length -1))
+  return [].concat(drivers.slice(0, drivers.length -1))
 }
 
 function removeFirstDriver() {
