580762e6029ba1e458321b5cee26b3c3acb7f29b
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-08-26 18:18:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   return drivers.push(name)
 }
+
+function destructivelyPrependDriver(name) {
+  return drivers.
+}
